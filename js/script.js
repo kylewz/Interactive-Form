db7f7e20f4a4b4c979c154e12965b8cc3a77ad2e
kwzvej <kyle.zvejnieks@mines.sdsmt.edu>
2022-04-16 15:26:21 -0600

    Script.js job role field
    "Other" job role field hidden or visible based on Job Role selector

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 const nameField = document.getElementById('name');
+const otherJobRoleField = document.getElementById('other-job-role');
+const jobRoleSelector = document.getElementById('title');
 
 // Default focus on name input on page load
 nameField.focus();
+
+// Hide "other" job role field on page load
+otherJobRoleField.style.display= 'none';
+
+// Listen for job role selector to show "other" field or not
+jobRoleSelector.addEventListener('change', (e) => {
+    if(e.target.value === 'other')
+        otherJobRoleField.style.display = '';
+    else
+        otherJobRoleField.style.display = 'none';
+    }
+);
