406fa7fb1d3094649629cf6c5242db3fe6efff1b
kwzvej <kyle.zvejnieks@mines.sdsmt.edu>
2022-04-19 10:48:27 -0600

    Update script.js
    Shirt color field enabled by shirt design selection, color options selectable based on chosen shirt design

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 const nameField = document.getElementById('name');
 const otherJobRoleField = document.getElementById('other-job-role');
 const jobRoleSelector = document.getElementById('title');
+const shirtDesignsSelector = document.getElementById('design');
+const shirtColorSelector = document.getElementById('color');
+const shirtColorOptions = document.querySelectorAll('#color option');
 
 // Default focus on name input on page load
 nameField.focus();
 
 // Hide "other" job role field on page load
-otherJobRoleField.style.display= 'none';
+otherJobRoleField.style.display = 'none';
 
 // Listen for job role selector to show "other" field or not
 jobRoleSelector.addEventListener('change', (e) => {
-    if(e.target.value === 'other')
-        otherJobRoleField.style.display = '';
-    else
-        otherJobRoleField.style.display = 'none';
+  if (e.target.value === 'other') otherJobRoleField.style.display = '';
+  else otherJobRoleField.style.display = 'none';
+});
+
+// Shirt Color selector disabled until design chosen
+shirtColorSelector.disabled = true;
+
+// Color options available dependendent on chosen shirt design
+shirtDesignsSelector.addEventListener('change', (e) => {
+  shirtColorSelector.disabled = false;
+
+  for (let i = 0; i < shirtColorOptions.length; i++) {
+    const shirtDesignSelected = e.target.value;
+    const colorForShirtDesign = shirtColorOptions[i].dataset.theme;
+
+    if (colorForShirtDesign === shirtDesignSelected) {
+      shirtColorOptions[i].hidden = false;
+    } else {
+      shirtColorOptions[i].hidden = true;
     }
-);
+  }
+});
